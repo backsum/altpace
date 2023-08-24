e6d760dbd93d37537c69233299f2debcb4528b1b
Aleksei Alekseev <aleksdaok@gmail.com>
2023-08-24 22:48:53 +0400

    v2
    Moved the repeated print function from main code to rolling_dice function.

--- assignment2 craps.py
+++ assignment2 craps.py
@@ -4,10 +4,10 @@ def rolling_dice() -> tuple:
     first_dice = randrange(1, 7)
     second_dice = randrange(1, 7)
     dice_sum = first_dice + second_dice
+    print(f"The sum of dice is {first_dice} + {second_dice} = {dice_sum}")
     return first_dice, second_dice, dice_sum
     
 first_dice, second_dice, dice_sum  = rolling_dice()
-print(f"The sum of dice is {first_dice} + {second_dice} = {dice_sum}")
 if dice_sum in [7, 11]:
     print("You won!")
 elif dice_sum in [2, 3, 12]:
@@ -17,7 +17,6 @@ else:
     print(f"Now your goal sum is {goal_sum}")
     while True:
         first_dice, second_dice, dice_sum  = rolling_dice()
-        print(f"The sum of dice is {first_dice} + {second_dice} = {dice_sum}")
         if dice_sum == 7:
             print("You lost!")
             break
